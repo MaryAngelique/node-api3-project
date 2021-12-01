103c3bca6ee700215a3ace9c9d175db13c338c8d
Angelique Abacajan <ma_utsey@yahoo.com>
2021-12-01 16:17:51 -0500

    Implemented get router to return a promise

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,14 +2,21 @@ const express = require('express');
 
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
+const User = require("./users-model");
+const Post = require("../posts/posts-model");
 
+const { validateUserId, validateUser, validatePost } = require("../middleware/middleware");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+router.get('/', (req, res, next) => {
+  User.get()
+    .then(users => {
+      res.json(users)
+
+    }).catch(next)
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
 });
@@ -19,26 +26,34 @@ router.post('/', (req, res) => {
   // this needs a middleware to check that the request body is valid
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',  validateUserId, (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
+
+router.use((error, req, res, next) => {
+  res.status(error.status || 500).json( { customMessage: "Router not working",
+    message: error.message,
+    stack: error.stack,
+ } )
+})
 
 // do not forget to export the router
