607557e13f1ee0a1fa795ffa8529ce4da329b80f
Angelique Abacajan <ma_utsey@yahoo.com>
2021-12-01 14:11:29 -0500

    Installed dependencies and launched server in index.js file

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // require your server and launch it
+const express = require("express");
+const server = express();
+
+const port = 9000;
+
+server.listen(() => {
+    console.log(`Listening on Port ${port}`);
+})
