632dc92c718f853118c5fe0d23dd8fdfcf202144
Angelique Abacajan <ma_utsey@yahoo.com>
2021-12-01 14:27:27 -0500

    Implemented server.js and completed logger middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,8 @@
+const User = require("../users/users-model");
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(req.method, req.url, Date.now() );
+  next();
 }
 
 function validateUserId(req, res, next) {
